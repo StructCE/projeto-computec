156d05b4e309a694aa88d32b4be001bef4a60f96
GuilhermeSams <guilhermeps3670@gmail.com>
2024-06-08 14:34:01 -0300

    NavBar Bottom feita

---
+++ mobile/app/(tabs)/anais.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "tamagui";
+
+export default function Anais() {
+  return (
+    <View flex={1} jc={"center"} ai={"center"}>
+      <Text>Anais</Text>
+    </View>
+  );
+}
---
+++ mobile/app/(tabs)/map.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "tamagui";
+
+export default function Tab() {
+  return (
+    <View flex={1} jc={"center"} ai={"center"}>
+      <Text>Mapa</Text>
+    </View>
+  );
+}
