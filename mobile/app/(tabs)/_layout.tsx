47dee7db0663d4a1839cf7b18e312e50487a9255
GuilhermeSams <guilhermeps3670@gmail.com>
2024-06-08 21:38:24 -0300

    Header

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,59 +1,62 @@
 import { Tabs } from "expo-router";
 import { Home, Map, ScrollText, BookOpenText } from "lucide-react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import Header from "@/src/components/Header";
 
 export default function TabNavigationLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "white",
-        tabBarInactiveTintColor: "#CBC8C8",
-        tabBarBackground: () => (
-          <LinearGradient
-            colors={["#a92227", "#ed7a17"]}
-            start={{ x: 0, y: 1 }}
-            locations={[0.4, 1]}
-            style={{
-              height: 70,
-            }}
-          />
-        ),
-        tabBarStyle: {
-          height: `${8}%`,
-          paddingBottom: `${2}%`,
-          paddingTop: `${2}%`,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Início",
-
-          tabBarIcon: ({ color }) => <Home size={30} color={color} />,
+    <>
+      <Tabs
+        screenOptions={{
+          header: Header,
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "#CBC8C8",
+          tabBarBackground: () => (
+            <LinearGradient
+              colors={["#a92227", "#ed7a17"]}
+              start={{ x: 0, y: 1 }}
+              locations={[0.4, 1]}
+              style={{
+                height: 70,
+              }}
+            />
+          ),
+          tabBarStyle: {
+            height: `${8}%`,
+            paddingBottom: `${2}%`,
+            paddingTop: `${2}%`,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="map"
-        options={{
-          title: "Mapa",
-          tabBarIcon: ({ color }) => <Map size={30} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="anais"
-        options={{
-          title: "Anais",
-          tabBarIcon: ({ color }) => <ScrollText size={30} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="news"
-        options={{
-          title: "Notícias",
-          tabBarIcon: ({ color }) => <BookOpenText size={30} color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Início",
+            tabBarIcon: ({ color }) => <Home size={30} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="map"
+          options={{
+            title: "Mapa",
+            tabBarIcon: ({ color }) => <Map size={30} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="anais"
+          options={{
+            title: "Anais",
+            tabBarIcon: ({ color }) => <ScrollText size={30} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="news"
+          options={{
+            title: "Notícias",
+            tabBarIcon: ({ color }) => <BookOpenText size={30} color={color} />,
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
