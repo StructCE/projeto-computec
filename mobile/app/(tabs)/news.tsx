f15f7d217de06d7d1d683ee76a080de268485384
leopcortes <leo.pereira.cortes@gmail.com>
2024-06-14 00:28:19 -0300

    carousel nas imagens dos posts

--- mobile/app/(tabs)/news.tsx
+++ mobile/app/(tabs)/news.tsx
@@ -6,27 +6,48 @@ import { ScrollView, Text, View, YStack } from 'tamagui';
 
 export default function News() {
   const post1 = {
-    image:
+    images: [
       'https://conteudo.imguol.com.br/c/noticias/1f/2022/01/21/hacker-usando-um-laptop-1642786034169_v2_4x3.jpg',
+      'https://ravel.com.br/blog/wp-content/uploads/2022/08/Hackers-do-dia-a-dia-Capa.jpg',
+      'https://static.vecteezy.com/ti/vetor-gratis/p3/25463773-hacker-logotipo-projeto-uma-misterioso-e-perigoso-hacker-ilustracao-vetor.jpg',
+      'https://t4.ftcdn.net/jpg/05/50/95/87/360_F_550958748_OeGcRonEUNoVhd0wjd9zSEMhLFIGO9Bt.jpg',
+    ],
     title: 'Evento hackerman cancelado!',
     subtitle: 'Está programação foi cancelada por problemas técnicos',
     date: '31/05',
   };
   const post2 = {
-    image:
+    images: [
       'https://wiki.inf.ufpr.br/computacao/lib/exe/fetch.php?w=400&tok=4a001d&media=https%3A%2F%2Fcdn.linuxfordevices.com%2Fwp-content%2Fuploads%2F2022%2F07%2Flinus-Torvalds.jpg',
+      'https://files.tecnoblog.net/wp-content/uploads/2018/09/linus-torvalds-dedo-meio.jpg',
+      'https://www.somenek.com.br/fx-files/images/big/11_plgNewsPoster.webp',
+    ],
     title: 'Criador do Linux presente no OS AI',
     subtitle: 'Criador do Linux estará presente no evento base, no dia 25/06.',
     date: '29/05',
   };
+  const post3 = {
+    images: [
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Campus_Darcy_Ribeiro_%2831398447555%29.jpg/1200px-Campus_Darcy_Ribeiro_%2831398447555%29.jpg',
+      'https://s2-g1.glbimg.com/n6BIBllBbGnO1gw3D0GoWvGRzok=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/W/M/KDLcjeRFaVVkBEMHgfcg/20220103-reitoria-betomonteiro-3.jpg',
+      'https://noticias.unb.br/images/Noticias/2023/08-Ago/20230425_campus-fga_valsdemir-carvalho_2.jpeg',
+      'https://noticias.unb.br/images/Noticias/2023/08-Ago/20230425_campus-fga_valsdemir-carvalho_2.jpeg',
+      'https://s2-g1.glbimg.com/WC-tK4BqijcmBKgQ2DRhR_CJzuc=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/7/n/AmVxkYQMCOoQhA133uPg/nota-universidade-00022-frame-63.jpeg',
+    ],
+    title: 'What is Lorem Ipsum?',
+    subtitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    date: '14/06',
+  };
 
   return (
     <ScrollView>
-      <YStack margin="$4" style={{ flex: 1, gap: 24 }}>
+      <YStack margin={16} style={{ flex: 1, gap: 16 }}>
         <PageTitle title="Notícias"></PageTitle>
         {/* ToDo: notícias vindas do banco de dados */}
         <NewsCard post={post1}></NewsCard>
         <NewsCard post={post2}></NewsCard>
+        <NewsCard post={post3}></NewsCard>
       </YStack>
     </ScrollView>
   );
--- mobile/src/components/NewsScreen/NewsCard.tsx
+++ mobile/src/components/NewsScreen/NewsCard.tsx
@@ -1,87 +1,145 @@
 import { BlurView } from 'expo-blur';
 import { Link } from 'expo-router';
-import { ImageBackground } from 'react-native';
-import { Button, Image, Text, View, YStack } from 'tamagui';
+import React, { useEffect, useRef } from 'react';
+import { Dimensions, ScrollView } from 'react-native';
+import { Image, Text, View, YStack } from 'tamagui';
 
 type Post = {
-  image: string;
+  images: string[];
   title: string;
   subtitle: string;
   date: string;
 };
 
+const { width, height } = Dimensions.get('window');
+
 export default function NewsCard({ post }: { post: Post }) {
+  const margin = 16; // Ajustar de acordo com a margin da YStack na página 'app/(tabs)/news.tsx'
+  const imageWidth = width - 2 * margin;
+
+  const scrollViewRef = useRef<any>(null);
+
+  useEffect(() => {
+    const numOfBackground = post.images.length;
+    let scrollValue = 0,
+      scrolled = 0;
+    const intervalId = setInterval(() => {
+      scrolled++;
+      if (scrolled < numOfBackground) scrollValue = scrollValue + imageWidth;
+      else {
+        scrollValue = 0;
+        scrolled = 0;
+      }
+      scrollViewRef.current?.scrollTo({ x: scrollValue, animated: true });
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <ImageBackground source={{ uri: post.image }} borderRadius={20}>
-      <YStack
+    <View style={{ flex: 1, alignSelf: 'center' }}>
+      <ScrollView ref={scrollViewRef} horizontal={true} pagingEnabled={true}>
+        {post.images.map((image) => {
+          return (
+            <Image
+              key={image}
+              source={{
+                uri: image,
+              }}
+              style={{
+                width: imageWidth,
+                height: height / 3,
+                borderRadius: 20,
+                padding: 12,
+              }}
+            />
+          );
+        })}
+      </ScrollView>
+      <View
         style={{
-          borderRadius: 20,
-          paddingVertical: 20,
-          paddingRight: 20,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
         }}
       >
-        <View
+        <YStack
           style={{
-            backgroundColor: 'white',
-            width: 76,
-            borderRadius: 9,
-            padding: 8,
-            alignItems: 'center',
-            alignSelf: 'flex-end',
+            borderRadius: 20,
+            paddingVertical: 16,
+            paddingRight: 16,
+            alignSelf: 'center',
+            width: imageWidth,
+            height: '100%',
           }}
         >
-          <Text>Postado</Text>
-          <Text
+          <View
             style={{
-              color: 'black',
-              fontWeight: 'bold',
+              backgroundColor: 'white',
+              borderRadius: 9,
+              padding: 8,
+              alignItems: 'center',
+              alignSelf: 'flex-end',
             }}
           >
-            {post.date}
-          </Text>
-        </View>
-        <BlurView
-          intensity={30}
-          style={{
-            width: 275,
-            overflow: 'hidden',
-            borderStartEndRadius: 20,
-            borderEndEndRadius: 20,
-            backgroundColor: 'rgba(0, 0, 0, 0.2)',
-            borderColor: 'rgba(0, 0, 0, 0.01)',
-            paddingLeft: 24,
-            paddingRight: 12,
-            paddingVertical: 16,
-            marginVertical: 12,
-            flex: 1,
-          }}
-        >
-          <YStack style={{ gap: 6 }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}>
-              {post.title}
+            <Text>Postado</Text>
+            <Text
+              style={{
+                color: 'black',
+                fontWeight: 'bold',
+              }}
+            >
+              {post.date}
             </Text>
-            <Text style={{ fontSize: 13, color: 'white' }}>
-              {post.subtitle}
-            </Text>
-          </YStack>
-        </BlurView>
-        <Link
-          href={{ pathname: '', params: {} }} // ToDo: link redirecionar para página do post
-          style={{
-            color: 'white',
-            backgroundColor: '#C1272D',
-            fontWeight: 'bold',
-            width: 132,
-            alignSelf: 'center',
-            textAlign: 'center',
-            paddingVertical: 10,
-            borderRadius: 4,
-            overflow: 'hidden',
-          }}
-        >
-          Leia Mais
-        </Link>
-      </YStack>
-    </ImageBackground>
+          </View>
+          <BlurView
+            intensity={30}
+            style={{
+              width: imageWidth - imageWidth / 5,
+              overflow: 'hidden',
+              borderStartEndRadius: 20,
+              borderEndEndRadius: 20,
+              backgroundColor: 'rgba(0, 0, 0, 0.2)',
+              borderColor: 'rgba(0, 0, 0, 0.01)',
+              paddingLeft: 24,
+              paddingRight: 12,
+              paddingVertical: 16,
+              marginVertical: 12,
+              flex: 1,
+            }}
+          >
+            <YStack style={{ gap: 6 }}>
+              <Text
+                style={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}
+              >
+                {post.title}
+              </Text>
+              <Text style={{ fontSize: 13, color: 'white' }}>
+                {post.subtitle}
+              </Text>
+            </YStack>
+          </BlurView>
+          <Link
+            href={{ pathname: '', params: {} }} // ToDo: link redirecionar para página do post
+            style={{
+              color: 'white',
+              backgroundColor: '#C1272D',
+              fontWeight: 'bold',
+              width: 132,
+              alignSelf: 'center',
+              marginLeft: margin,
+              textAlign: 'center',
+              paddingVertical: 10,
+              borderRadius: 4,
+              overflow: 'hidden',
+            }}
+          >
+            Leia Mais
+          </Link>
+        </YStack>
+      </View>
+    </View>
   );
 }
