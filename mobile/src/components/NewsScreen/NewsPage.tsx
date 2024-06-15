8d7e88ac501256005a4cb1d52b30c8de366adebc
leopcortes <leo.pereira.cortes@gmail.com>
2024-06-14 23:08:06 -0300

    carrossel passando de imagens por meio das setas

--- mobile/src/components/NewsScreen/NewsPage.tsx
+++ mobile/src/components/NewsScreen/NewsPage.tsx
@@ -1,13 +1,13 @@
 import { BlurView } from 'expo-blur';
 import { useLocalSearchParams } from 'expo-router';
-import React, { useEffect, useRef } from 'react';
-import { Dimensions, ScrollView } from 'react-native';
+import { ArrowLeft, ArrowRight } from 'lucide-react-native';
+import React, { useRef, useState } from 'react';
+import { Dimensions, ScrollView, TouchableOpacity } from 'react-native';
 import { Image, Text, View, XStack, YStack } from 'tamagui';
 
 const { width, height } = Dimensions.get('window');
 
 export default function NewsPage() {
-  const params = useLocalSearchParams();
   const {
     images = [],
     title,
@@ -17,29 +17,33 @@ export default function NewsPage() {
     time,
     local,
     posted,
-  } = params;
+  } = useLocalSearchParams(); // Parametros passados pelo Link do expo-router
 
-  let imageArray = Array.isArray(images) ? images : [images];
-  imageArray = imageArray[0].split(',');
+  let imageArray = Array.isArray(images) ? images : [images]; // Corrige erro do map (possivelmente undefined)
+  imageArray = imageArray[0].split(','); // Separa os links ['link1,link2,link3'] -> ['link1','link2','link3']
 
   const scrollViewRef = useRef<any>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const numOfBackground = images!.length;
-    let scrollValue = 0,
-      scrolled = 0;
-    const intervalId = setInterval(() => {
-      scrolled++;
-      if (scrolled < numOfBackground) scrollValue = scrollValue + width;
-      else {
-        scrollValue = 0;
-        scrolled = 0;
-      }
-      scrollViewRef.current?.scrollTo({ x: scrollValue, animated: true });
-    }, 5000);
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      scrollViewRef.current?.scrollTo({
+        x: (currentIndex - 1) * width,
+        animated: true,
+      });
+    }
+  };
 
-    return () => clearInterval(intervalId);
-  }, []);
+  const handleNext = () => {
+    if (currentIndex < imageArray.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      scrollViewRef.current?.scrollTo({
+        x: (currentIndex + 1) * width,
+        animated: true,
+      });
+    }
+  };
 
   return (
     <ScrollView style={{ flex: 1, gap: 16 }}>
@@ -112,6 +116,7 @@ export default function NewsPage() {
                 paddingRight: 12,
                 paddingVertical: 20,
                 marginVertical: 12,
+                marginBottom: 20,
               }}
             >
               <YStack style={{ gap: 6 }}>
@@ -123,14 +128,47 @@ export default function NewsPage() {
                 <Text style={{ fontSize: 13, color: 'white' }}>{subtitle}</Text>
               </YStack>
             </BlurView>
+            <XStack
+              style={{
+                justifyContent: 'space-between',
+                width: width,
+                position: 'absolute',
+                bottom: 16,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  padding: 5,
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  borderRadius: 25,
+                  left: 16,
+                }}
+                onPress={handlePrev}
+              >
+                <ArrowLeft size={22} color="white" strokeWidth={2} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  padding: 5,
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  borderRadius: 25,
+                  right: 16,
+                }}
+                onPress={handleNext}
+              >
+                <ArrowRight size={22} color="white" strokeWidth={2} />
+              </TouchableOpacity>
+            </XStack>
           </YStack>
         </View>
       </View>
-      <YStack style={{ margin: 16 }}>
+      <YStack style={{ margin: 20 }}>
         <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#1A1A1A' }}>
           Detalhes
         </Text>
-        <Text style={{ marginVertical: 12 }}>{description}</Text>
+        <Text style={{ marginVertical: 12, textAlign: 'justify' }}>
+          {description}
+        </Text>
         <XStack style={{ gap: 4, marginVertical: 1 }}>
           <Text>Dia:</Text>
           <Text style={{ fontWeight: 'bold' }}>{date}</Text>
