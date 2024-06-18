6bc444481f5b7e140688b38363b12d9aabfadccb
leopcortes <leo.pereira.cortes@gmail.com>
2024-06-18 16:39:10 -0300

    PopoverDelete post

---
+++ mobile/src/components/Admin/PopoverDelete.tsx
@@ -0,0 +1,93 @@
+import { Trash2 } from 'lucide-react-native';
+import type { PopoverProps } from 'tamagui';
+import { Adapt, Button, Label, Popover, Text, XStack, YStack } from 'tamagui';
+
+type Post = {
+  images: string[];
+  title: string;
+  subtitle: string;
+  description: string;
+  date: string;
+  time: string;
+  local: string;
+  posted: string;
+};
+
+export default function PopoverDelete({
+  post,
+  ...props
+}: PopoverProps & { post: Post }) {
+  return (
+    <Popover size="$5" allowFlip {...props}>
+      <Popover.Trigger asChild style={{ backgroundColor: 'lightgray' }}>
+        <Button icon={Trash2} color={'red'} size={48} />
+      </Popover.Trigger>
+      <Adapt when="sm" platform="touch">
+        <Popover.Sheet snapPointsMode={'fit'} modal dismissOnSnapToBottom>
+          <Popover.Sheet.Frame padding="$4">
+            <Adapt.Contents />
+          </Popover.Sheet.Frame>
+          <Popover.Sheet.Overlay
+            animation="lazy"
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+        </Popover.Sheet>
+      </Adapt>
+      <Popover.Content
+        borderWidth={1}
+        enterStyle={{ y: -10, opacity: 0 }}
+        exitStyle={{ y: -10, opacity: 0 }}
+        elevate
+        animation={[
+          'quick',
+          {
+            opacity: {
+              overshootClamping: true,
+            },
+          },
+        ]}
+      >
+        <YStack gap="$3">
+          <YStack>
+            <Label style={{ fontWeight: 'bold', fontSize: 24 }}>
+              Deseja deletar a postagem?
+            </Label>
+            <Text style={{ fontSize: 16 }}>
+              O conteúdo da postagem "
+              <Text color={'#C1272D'} fontWeight={600}>
+                {post.title}
+              </Text>
+              " será excluído e a ação não pode ser desfeita.
+            </Text>
+          </YStack>
+          <XStack
+            style={{
+              flex: 1,
+              justifyContent: 'space-evenly',
+              gap: 24,
+              marginBottom: 96,
+            }}
+          >
+            <Popover.Close asChild>
+              <Button style={{ fontSize: 14, flex: 1 }}>Cancelar</Button>
+            </Popover.Close>
+            <Button
+              style={{
+                backgroundColor: '#ED7A17',
+                color: 'white',
+                flex: 1,
+                fontWeight: 'bold',
+              }}
+              onPress={() => {
+                /* Código para deletar postagem */
+              }}
+            >
+              Deletar
+            </Button>
+          </XStack>
+        </YStack>
+      </Popover.Content>
+    </Popover>
+  );
+}
