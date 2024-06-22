e2e07d0df91630f570e1b058533475df7851d767
MatheusNevs <matheusnf62@gmail.com>
2024-06-22 01:05:38 -0300

    loading fonts

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,11 +5,22 @@ import tamaguiConfig from "@/tamagui.config";
 import { AuthProvider } from "@/utils/auth";
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
-//import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import {
+  MavenPro_400Regular,
+  MavenPro_600SemiBold,
+  MavenPro_500Medium,
+  MavenPro_700Bold,
+} from "@expo-google-fonts/maven-pro";
+
 export default function RootLayout() {
   const [interLoaded, interError] = useFonts({
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+    InterMedium: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    MavenProRegular: MavenPro_400Regular,
+    MavenProMedium: MavenPro_500Medium,
+    MavenProSemiBold: MavenPro_600SemiBold,
+    MavenProBold: MavenPro_700Bold,
   });
 
   useEffect(() => {
