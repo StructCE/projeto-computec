92c993327e39302d6466fcb65c56f23f717e3778
MatheusNevs <matheusnf62@gmail.com>
2024-06-22 01:09:37 -0300

    login page integration with auth and style/fonts changes

--- mobile/app/(tabs)/admin/login.tsx
+++ mobile/app/(tabs)/admin/login.tsx
@@ -1,33 +1,46 @@
+import React, { useEffect } from "react";
 import { View, Text, XStack, Image } from "tamagui";
 import { TouchableOpacity } from "react-native";
-import { useFonts } from "expo-font";
-import { StyleSheet } from "react-native";
-import {
-  MavenPro_600SemiBold,
-  MavenPro_400Regular,
-} from "@expo-google-fonts/maven-pro";
+import { useAuth } from "@/utils/auth";
+import { router } from "expo-router";
 
 export default function Login() {
-  const [fontsLoaded] = useFonts({
-    MavenPro_600SemiBold,
-    MavenPro_400Regular,
-  });
-  if (!fontsLoaded) {
-    return;
-  }
+  const { signIn, userSession } = useAuth();
+
+  useEffect(() => {
+    if (userSession) {
+      router.push("/");
+    }
+  }, [userSession]);
 
   return (
     <View flex={1} jc={"center"} mb="$10" ai={"center"}>
       <View ai={"center"} mb="$12">
-        <Text fontSize={"$9"} color={"#ED7A17"} style={styles.mavenProSemiBold}>
+        <Text
+          fontSize={"$9"}
+          color={"#ED7A17"}
+          style={{
+            fontFamily: "MavenProSemiBold",
+          }}
+        >
           Login
         </Text>
-        <Text style={styles.mavenProRegular} mt={"$-1"} color={"1A1A1A"}>
+        <Text
+          style={{
+            fontFamily: "MavenProRegular",
+          }}
+          mt={"$-1"}
+          color={"1A1A1A"}
+        >
           para membros internos
         </Text>
       </View>
 
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          signIn("google");
+        }}
+      >
         <XStack
           br={8}
           ai={"center"}
@@ -41,7 +54,11 @@ export default function Login() {
             h={24}
             source={require("../../../../mobile/assets/images/google-icon.png")}
           />
-          <Text fontSize="$4" ta={"center"} style={{ fontWeight: "bold" }}>
+          <Text
+            fontSize="$4"
+            ta={"center"}
+            style={{ fontWeight: "bold", fontFamily: "InterMedium" }}
+          >
             Entrar com Google
           </Text>
           <View w={24} h={24}></View>
@@ -51,14 +68,20 @@ export default function Login() {
       <View my={"$2.5"} fd={"row"} ai={"center"}>
         <View marginRight={"$2"} w={"20%"} height={1} bg={"#E6E6E6"} />
         <View>
-          <Text style={styles.mavenProRegular} ta={"center"} color={"#B3B3B3"}>
+          <Text
+            style={{
+              fontFamily: "MavenProRegular",
+            }}
+            ta={"center"}
+            color={"#B3B3B3"}
+          >
             ou continue como
           </Text>
         </View>
         <View ml={"$2"} w={"20%"} height={1} bg={"#E6E6E6"} />
       </View>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => signIn("github")}>
         <XStack
           br={8}
           ai={"center"}
@@ -72,7 +95,11 @@ export default function Login() {
             h={24}
             source={require("../../../../mobile/assets/images/github-icon.png")}
           />
-          <Text fontSize="$4" ta={"center"} style={{ fontWeight: "bold" }}>
+          <Text
+            fontSize="$4"
+            ta={"center"}
+            style={{ fontWeight: "bold", fontFamily: "InterMedium" }}
+          >
             Entrar com Github
           </Text>
           <View w={24} h={24}></View>
@@ -83,17 +110,37 @@ export default function Login() {
 
       <View paddingHorizontal="$6">
         <Text ta={"center"}>
-          <Text style={styles.mavenProRegular} color={"#B3B3B3"}>
+          <Text
+            style={{
+              fontFamily: "MavenProRegular",
+            }}
+            color={"#B3B3B3"}
+          >
             destinado à
           </Text>
-          <Text style={styles.mavenProRegular} color={"#1A1A1A"}>
+          <Text
+            style={{
+              fontFamily: "MavenProRegular",
+            }}
+            color={"#1A1A1A"}
+          >
             {" "}
             administração{" "}
           </Text>
-          <Text style={styles.mavenProRegular} color={"#B3B3B3"}>
+          <Text
+            style={{
+              fontFamily: "MavenProRegular",
+            }}
+            color={"#B3B3B3"}
+          >
             do aplicativo somente membros
           </Text>
-          <Text style={styles.mavenProRegular} color={"#1A1A1A"}>
+          <Text
+            style={{
+              fontFamily: "MavenProRegular",
+            }}
+            color={"#1A1A1A"}
+          >
             {" "}
             permitidos.
           </Text>
@@ -102,13 +149,3 @@ export default function Login() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  mavenProSemiBold: {
-    fontFamily: "MavenPro_600SemiBold",
-  },
-
-  mavenProRegular: {
-    fontFamily: "MavenPro_400Regular",
-  },
-});
