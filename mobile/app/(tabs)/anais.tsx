4f74003aa183c35b7f92c91954949d7abbb3e673
MauroSon <mcontaalternativa@gmail.com>
2024-06-24 01:30:35 -0300

    Adding Gradient

--- mobile/app/(tabs)/anais.tsx
+++ mobile/app/(tabs)/anais.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from "tamagui";
+import { View, Text, XStack, YStack } from "tamagui";
+import { Link } from "expo-router"
+import { LinearGradient } from "expo-linear-gradient";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 export default function Anais() {
   return (
-    <View flex={1} jc={"center"} ai={"center"}>
-      <Text>Anais</Text>
-    </View>
-  );
+    <MaskedView
+      maskElement={
+        <Text
+          fontSize={20}
+          style={{
+            fontFamily: "MavenProBold",
+          }}
+        >
+          ANAIS DE CONFERÊNCIA
+        </Text>
+      }
+    >
+      <LinearGradient
+        colors={["#a92227", "#ed7a17"]}
+        start={{ x: 0.5, y: 2 }}
+        end={{ x: 0.5, y: -0.5 }}
+        locations={[0.4, 1]}
+        style={{
+          height: 30,
+        }}
+      />
+    </MaskedView> 
+  )
 }
