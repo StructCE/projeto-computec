666a93d5efcaa940716dda0d25f5855d7b87b79e
MatheusNevs <matheusnf62@gmail.com>
2024-06-24 18:47:35 -0300

    notify function and settings

--- express-trpc-prisma/src/trpc/routes/post/procedures/createPost.ts
+++ express-trpc-prisma/src/trpc/routes/post/procedures/createPost.ts
@@ -1,6 +1,42 @@
 import { db } from "../../../../db";
 import z from "zod";
 import { protectedProcedure } from "../../../trpc";
+import Expo, { ExpoPushMessage } from "expo-server-sdk";
+
+export const notify = async ({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle: string;
+}) => {
+  const expo = new Expo();
+  const pushTokens = await db.pushToken.findMany();
+
+  let notifications: ExpoPushMessage[] = [];
+  for (let pushToken of pushTokens) {
+    if (!Expo.isExpoPushToken(pushToken.token)) {
+      console.error(
+        `Push token ${pushToken.token} is not a valid Expo push token`
+      );
+    }
+    console.log(pushToken.token);
+    notifications.push({
+      to: pushToken.token,
+      sound: "default",
+      title: title,
+      body: subtitle,
+    });
+  }
+  let chunks = expo.chunkPushNotifications(notifications);
+  for (let chunk of chunks) {
+    try {
+      await expo.sendPushNotificationsAsync(chunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
 
 export const createPost = protectedProcedure
   .input(
@@ -9,8 +45,8 @@ export const createPost = protectedProcedure
         title: z.string(),
         subtitle: z.string(),
         description: z.string(),
-        local: z.string(),
-        dateTime: z.date(),
+        local: z.string().optional(),
+        dateTime: z.date().optional(),
         images: z.array(z.string()),
       }),
     })
@@ -28,5 +64,11 @@ export const createPost = protectedProcedure
         },
       });
     });
-    return createdPost;
+    const createdNotification = await db.notification.create({
+      data: {
+        post_id: createdPost.id,
+      },
+    });
+    await notify(data);
+    return { post: createdPost, notification: createdNotification };
   });
---
+++ mobile/utils/notifications.tsx
@@ -0,0 +1,112 @@
+import { useState, useEffect, useRef } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+import { getBaseUrl } from "./api";
+import Constants from "expo-constants";
+
+export const usePushNotifications = () => {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: false,
+      shouldShowAlert: true,
+      shouldSetBadge: false,
+    }),
+  });
+
+  const [expoPushToken, setExpoPushToken] = useState<
+    Notifications.ExpoPushToken | undefined
+  >();
+
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >();
+
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  async function registerForPushNotificationsAsync() {
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification");
+        return;
+      }
+
+      const newToken = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      });
+      const oldToken = SecureStore.getItem("pushToken");
+      if (newToken.data !== oldToken) {
+        SecureStore.setItem("pushToken", newToken.data);
+      }
+      return {
+        newToken,
+        oldToken,
+      };
+    } else {
+      alert("Must be using a physical device for Push notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    return;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      if (token) {
+        setExpoPushToken(token?.newToken);
+        fetch(`http://${getBaseUrl().split(":")[1]}:3001/setPushToken`, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            newToken: token.newToken.data,
+            oldToken: token.oldToken,
+          }),
+        });
+      }
+    });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current!
+      );
+
+      Notifications.removeNotificationSubscription(responseListener.current!);
+    };
+  }, []);
+
+  return {
+    expoPushToken,
+    notification,
+  };
+};
