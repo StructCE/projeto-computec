1939212e78e13a03ca56f9eb6e4e799e54d43714
MatheusNevs <matheusnf62@gmail.com>
2024-07-18 14:40:19 -0300

    cors and seed updated

--- express-trpc-prisma/src/server.ts
+++ express-trpc-prisma/src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 import { tRPCRouter } from "./trpc/middleware";
 import { authRouter } from "./auth";
 import { setPushTokenRoute } from "./utils/notifications/setPushTokenRoute";
+import cors from "cors";
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
